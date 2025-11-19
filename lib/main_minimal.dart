def09162f103888728b5a540d0ba83c85c342862
Your Name <you@example.com>
2025-11-19 08:35:02 -0700

    chore: update deps and ios bundle id

---
+++ lib/main_minimal.dart
@@ -0,0 +1,11 @@
+﻿import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text('TWM minimal test'),
+      ),
+    ),
+  ));
+}
